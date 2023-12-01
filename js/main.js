80f59e19e7616071869a3dcdc2a2df2e2d417448
mollan_ <146721224+mollan-gl@users.noreply.github.com>
2023-12-01 18:25:11 +0300

    updated todoApp

--- js/main.js
+++ js/main.js
@@ -79,7 +79,6 @@ function checkEmptyList() {
   if (tasks.length === 0) {
     const emptyListHTML = `
     <li id="emptyList" class="list-group-item empty-list">
-            <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3" />
             <div class="empty-list__title">Список дел пуст</div>
           </li>`;
     tasksList.insertAdjacentHTML("afterbegin", emptyListHTML);
